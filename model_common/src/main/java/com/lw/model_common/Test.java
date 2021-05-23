7996ded4cde1a6b5d92f40b4a0e88af9494a7da9
陆威 <404474566@qq.com>
2021-05-23 22:51:00 +0800

    查找原因arouter

--- model_common/src/main/java/com/lw/model_common/Test.java
+++ model_common/src/main/java/com/lw/model_common/Test.java
@@ -1,10 +1,19 @@
 package com.lw.model_common;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.alibaba.android.arouter.launcher.ARouter;
 
 /**
  * Created by lw on 2021/5/23.
  */
 
 public class Test extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ARouter.openLog();
+    }
 }
